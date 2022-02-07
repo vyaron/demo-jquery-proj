5f15fc5d2c973f4ae1395470b6c89a1044b26d8c
vyaron <vyaron@gmail.com>
2022-02-07 11:57:52 +0200

    Changes before customer demo - approved by Sasha *. Changed default ID to 10 *.  Added comment in service *. Landing page changes

--- js/storage-service.js
+++ js/storage-service.js
@@ -1,5 +1,7 @@
 'use strict';
 
+// StorageService
+
 function saveToStorage(key, val) {
     localStorage.setItem(key, JSON.stringify(val))
 }
--- js/util-service.js
+++ js/util-service.js
@@ -1,6 +1,6 @@
 'use strict';
 
-function makeId(length = 6) {
+function makeId(length = 10) {
     var txt = '';
     var possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
 
